0b2bea1c5a61fb7694665c99028145f44c994824
sf-mensch <sf-mensch@ed166372-6744-4ac0-a67f-bb1ae9efa102>
2017-11-01 23:24:05 +0000

    E.texi: fixed nonprintable char -> "fl" gnucobpg.texi: Fixed typos in references
    build-manuals.sh: added processing with makeinfo as this shows important errors (and nice to fix warnings)
    
    moved files in GNU Info format to sub-directory "info"
    
    regenerated results
    
    adjusted svn:ignore

--- external-doc/guide/build-manuals.sh
+++ external-doc/guide/build-manuals.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
-texi2pdf -q -c gnucobpg-a4.texi
-texi2pdf -q -c gnucobpg-letter.texi
-texi2pdf -q -c gnucobqr-a4.texi
-texi2pdf -q -c gnucobqr-letter.texi
-texi2pdf -q -c gnucobsp-a4.texi
-texi2pdf -q -c gnucobsp-letter.texi
+#echo
+#echo "GC manual build (PDF)"
+#mkdir -p PDFs
+#cd PDFs
+#echo   "Building Programmer's Guide"...
+#texi2pdf -q -c ../gnucobpg-a4.texi
+#texi2pdf -q -c ../gnucobpg-letter.texi
+#echo   "Building Quick Reference"...
+#texi2pdf -q -c ../gnucobqr-a4.texi
+#texi2pdf -q -c ../gnucobqr-letter.texi
+#echo   "Building Sample Programs"...
+#texi2pdf -q -c ../gnucobsp-a4.texi
+#texi2pdf -q -c ../gnucobsp-letter.texi
+#cd ..
+#echo
+#
+#echo "GC manual build (GNU Info)"
+#mkdir -p info
+#cd info
+#makeinfo --no-split ../gnucobpg.texi
+#makeinfo --no-split ../gnucobqr.texi
+#makeinfo --no-split ../gnucobsp.texi
+#cd ..
+#echo
+#
+#echo "GC manual build (HTML splitted)"
+#mkdir -p HTML
+#cd HTML
+#makeinfo --html --output=pg ../gnucobpg.texi
+#makeinfo --html --output=qr ../gnucobqr.texi
+#makeinfo --html --output=sp ../gnucobsp.texi
+#cd ..
+#echo
+
+echo "GC manual build (HTML monolitic)"
+mkdir -p HTML
+cd HTML
+makeinfo --html --no-headers --no-split ../gnucobpg.texi
+makeinfo --html --no-headers --no-split ../gnucobqr.texi
+makeinfo --html --no-headers --no-split ../gnucobsp.texi
+cd ..
+echo
+
 echo "GC manual build complete"
 exit 0
