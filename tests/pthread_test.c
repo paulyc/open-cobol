a9adcb180e9a6ad0199ed53bc80f85d09c6c3e10
gnoe <gnoe@ed166372-6744-4ac0-a67f-bb1ae9efa102>
2018-10-04 03:56:56 +0000

    Checks LMDB for support of process-shared mutexes.

---
+++ tests/pthread_test.c
@@ -0,0 +1,23 @@
+/*
+	Tests LMDB for process shared mutex functionality.
+	If this program returns 95, then LMDB must be recompiled
+  using the -DMDB_USE_POSIX_SEM cppflag.
+*/
+#include <stdlib.h>
+#include <sys/stat.h>
+#include "lmdb.h"
+
+int 
+main(int argc,char * argv[])
+{
+  int rc = 0;
+  MDB_env *env;
+  mkdir("/tmp/tmpdb", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); 
+  mdb_env_create(&env);
+  rc = mdb_env_open(env, "/tmp/tmpdb", 0, 0664);
+  remove("/tmp/tmpdb/data.mdb");
+  remove("/tmp/tmpdb/lock.mdb");
+  rmdir("/tmp/tmpdb");
+  return rc;
+}
+
